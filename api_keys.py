beb18f82e25fe1061ea9be8437619c2b87b74687
rromang <80008461+rromang@users.noreply.github.com>
2021-04-25 22:10:51 -0700

    data for track id/genre, dfs for every month

---
+++ api_keys.py
@@ -0,0 +1,9 @@
+# OpenWeatherMap API Key
+weather_api_key = "YOUR KEY HERE!"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
+
+#spotify
+client_id = "YOUR KEY HERE!"
+client_secret = "YOUR KEY HERE!"
