e4ae4056f49937c67d3f1ae43ca9ae80de033914
rromang <80008461+rromang@users.noreply.github.com>
2021-04-29 22:01:11 -0700

    organize and cleaned duplicate files

--- api_keys.py
+++
@@ -1,9 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
-
-# Google API Key
-g_key = "YOUR KEY HERE!"
-
-#spotify
-client_id = "YOUR KEY HERE!"
-client_secret = "YOUR KEY HERE!"
